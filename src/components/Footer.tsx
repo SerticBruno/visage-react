7dbb58ac298f66359b5383a89378119ea4ff4c2d
Bruno Sertić <sertic.bruno@gmail.com>
2025-05-10 21:04:25 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaWhatsapp, FaInstagram, FaEnvelope, FaCcVisa, FaCcMasterca
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-b from-gray-900 to-gray-800 text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-12 pb-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
           {/* Newsletter */}
           <div className="lg:col-span-2">
