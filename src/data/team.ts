da6d92a36cb29c9ae5f09d8b5361777e40f56f66
Bruno Sertić <sertic.bruno@gmail.com>
2025-07-18 13:38:24 +0200

    Update team.ts

--- src/data/team.ts
+++ src/data/team.ts
@@ -36,7 +36,7 @@ export const teamMembers: TeamMember[] = [
 export const teamDetails = {
   tatjana: 'Tatjana Torinek je diplomirala 1992. godine na Medicinskom fakultetu u Zagrebu, a od tada se bavi obiteljskom medicinom. Završila je specijalizaciju iz Obiteljske medicine i uspješno vodi svoju ambulantu već 18 godina. Estetskom medicinom se počela baviti prije 4 godine te se specijalizirala za obavljanje nekirurških estetskih tretmana. Prošla je brojne edukacije i tečajeve te redovito prisustvuje kongresima i predavanjima iz estetske medicine.',
   mia: 'Mia Torinek je diplomirala marketing na Ekonomskom fakultetu u Zagrebu. Osnovala je Visage studio 2023. godine skupa s majkom Tatjanom. 2024. godine je završila prekvalifikaciju za kozmetičara te se specijalizira za obavljanje neinvazivnih estetskih tretmana. Redovito odlazi na edukacije, tečajeve, radionice i kongrese iz estetske medicine te je uvijek u toku s trendovima.',
-  helena: 'Helena Torinek je diplomirala marketing na Ekonomskom fakultetu u Zagrebu. Specijalizira se za obavljanje neinvazivnih estetskih tretmana. Redovito odlazi na edukacije, tečajeve, radionice i kongrese iz estetske medicine te je uvijek u toku s trendovima. Omiljeni tretmani: PRP, mezoterapija polinukleotidima i skin boosteri'
+  helena: 'Helena Torinek je završila veterinu u Zagrebu. S medicinskim obrazovanjem i strasti prema estetskoj medicini, specijalizirala se za obavljanje neinvazivnih estetskih tretmana. Redovito odlazi na edukacije, tečajeve, radionice i kongrese iz estetske medicine te je uvijek u toku s najnovijim trendovima i tehnikama. Posvećena je pružanju sigurnih i učinkovitih tretmana, kombinirajući svoje medicinsko znanje s najmodernijim estetskim pristupima.'
 };
 
 export const teamDetailsSection = {
