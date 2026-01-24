a1e3f234a60c71a84bf207f016e46e5b6f32eaa2
Bruno Sertić <sertic.bruno@gmail.com>
2026-01-24 11:04:07 +0100

    text

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,7 +1,7 @@
 export const storyContent = {
   title: 'Naša priča',
   paragraphs: [
-    'Utemeljeni smo 2023. godine u Sisku. Visage studio je estetski studio specijaliziran za nekiruršku estetsku medicinu. U Visage studiju smo posvećeni regeneriranju kože, poboljšanju zdravlja kože i zaustavljanju znakova starenja. Nudimo individualni pristup i tretmane prilagođavamo vašim željama i potrebama.',
+    'Utemeljeni smo 2023. godine u Sisku. Visage studio je estetski studio specijaliziran za nekiruršku estetsku medicinu. Posvećeni smo regeneriranju kože, poboljšanju zdravlja kože i zaustavljanju znakova starenja. Nudimo individualni pristup i tretmane prilagođavamo vašim željama i potrebama.',
     'Uporabom najsuvremenijih tehnologija i metoda, Visage studio pruža najnovije tretmane i zahvate za regeneriranje kože. Kod nas, prvi put u Sisku, možete napraviti bleferoplazmu, postupak uklanjanja viška kože s gornjeg i donjeg kapka korištenjem tehnologije frakcijske plazme.',
     'Također, nudimo mezoterapiju Dermapenom 4 i Mesoject Gunom te možemo tretirati čak i najosjetljiviju kožu. U Visage studiju nudimo mezoterapiju egzosomima i polinukleotidima te aktiviramo prirodnu sposobnost kože da se sama regenerira',
     'U našem studiju možete, uz savjet kozmetologinje, kupiti dermokozmetičke proizvode i suplemente za njegu lica, tijela i vlasišta renomiranih svjetskih marki.'
